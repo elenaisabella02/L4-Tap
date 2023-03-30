aa52b8f4ee04ac5daff7b4b637942b8a61598d96
elenaisabella02 <bragaelenaisabella@yahoo.com>
2023-03-30 12:53:31 +0300

    CarRepository class

---
+++ Program.cs
@@ -0,0 +1,61 @@
+
+public class Car{
+	int Id;
+	string Brand;
+	string Model;
+	bool IsElectric;
+	int MaxSpeedKmh;
+	int AutonomyKm;
+}
+List<Car> carList = new List<Car> {
+new Car
+{
+	Id = 1,
+	Brand = "Dacia",
+	Model = "Spring",
+	IsElectric = true,
+	Autonomy = 300,
+	MaxSpeedKmh = 120
+},
+new Car
+{
+	Id = 2,
+	Brand = "VW",
+	Model = "Polo",
+	IsElectric = false,
+	Autonomy = 850,
+	MaxSpeedKmh = 180
+},
+new Car
+{
+	Id = 3,
+	Brand = "Ford",
+	Model = "Focus",
+	IsElectric = false,
+	Autonomy = 950,
+	MaxSpeedKmh = 210
+},
+new Car
+{
+	Id = 4,
+	Brand = "Tesla",
+	Model = "Model 3",
+	IsElectric = true,
+	Autonomy =
+500,
+	MaxSpeedKmh = 260
+},
+new Car
+{
+	Id = 5,
+	Brand = "Opel",
+	Model = "Astra",
+	IsElectric = false,
+	Autonomy = 1010,
+	MaxSpeedKmh = 190
+}
+}
+public CarRepository GetAll()
+{
+	return new List<Car>;
+}
