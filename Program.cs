61325a2e1d307ed066c01d0412fdc9862be0dc4e
elenaisabella02 <bragaelenaisabella@yahoo.com>
2023-03-30 16:59:30 +0300

    Created CarRepository class

--- Program.cs
+++ Program.cs
@@ -57,6 +57,66 @@ new Car
 }
 public class CarRepository GetAll()
 {
-	return new List<Car>;
+    public static List<Car> GetAll()
+    {
+        return new List<Car>
+        {
+            new Car 
+            { 
+                Id = 1, 
+                Brand = "Dacia", 
+                Model = "Spring", 
+                IsElectric = true, 
+                AutonomyKm = 300, 
+                MaxSpeedKmh = 120 
+            },
+            new Car 
+            { 
+                Id = 2, 
+                Brand = "VW", 
+                Model = "Polo", 
+                IsElectric = false, 
+                AutonomyKm = 850, 
+                MaxSpeedKmh = 180 },
+            new Car 
+            { 
+                Id = 3, 
+                Brand = "Ford", 
+                Model = "Focus", 
+                IsElectric = false, 
+                AutonomyKm = 950, 
+                MaxSpeedKmh = 210 
+            },
+            new Car 
+            { 
+                Id = 4, 
+                Brand = "Tesla", 
+                Model = "Model 3", 
+                IsElectric = true, 
+                AutonomyKm = 500, 
+                MaxSpeedKmh = 260 
+            },
+            new Car 
+            { 
+                Id = 5, 
+                Brand = "Opel", 
+                Model = "Astra", 
+                IsElectric = false, 
+                AutonomyKm = 1010, 
+                MaxSpeedKmh = 190 
+            }
+
+        };
+    }
 }
-class CarService: CarRepository
+
+
+
+
+
+
+
+
+
+
+
